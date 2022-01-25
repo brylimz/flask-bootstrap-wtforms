c01bc1e5ad11dfea99644940ab69d983d9a087a3
brylimz <superuser@brylimz.com>
2022-01-25 15:02:32 +0700

    validator email

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 from flask import Flask, render_template
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, validators
+from wtforms import StringField, PasswordField, SubmitField, validators, EmailField
 
 
 class LoginForm(FlaskForm):
-    email = StringField('Email', [validators.data_required()])
+    email = EmailField('Email', [validators.data_required(), validators.Email()])
     password = PasswordField('Password', [validators.data_required()])
     submit = SubmitField("Log In")
-
 
 
 app = Flask(__name__)
@@ -22,6 +21,7 @@ def home():
 @app.route("/login")
 def login():
     login_form = LoginForm()
+    login_form.validate_on_submit()
     return render_template('login.html', form=login_form)
 
 
