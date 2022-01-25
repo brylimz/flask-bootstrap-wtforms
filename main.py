1f76b30a57a67461ac16aafb3df04688a1646dd1
Bryan`Y. Limz <superuser@brylimz.com>
2022-01-25 21:20:34 +0700

    setup pc

--- main.py
+++ main.py
@@ -3,8 +3,12 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, validators, EmailField
 
 
+EMAIL = "admin@email.com"
+PASSWORD = "12345678"
+
+
 class LoginForm(FlaskForm):
-    email = EmailField('Email', [validators.data_required(), validators.Email()])
+    email = StringField('Email', [validators.data_required()])
     password = PasswordField('Password', [validators.data_required()])
     submit = SubmitField("Log In")
 
@@ -18,10 +22,14 @@ def home():
     return render_template('index.html')
 
 
-@app.route("/login")
+@app.route("/login", methods=["GET", "POST"])
 def login():
     login_form = LoginForm()
-    login_form.validate_on_submit()
+    if login_form.validate_on_submit():
+        if login_form.email.data == "admin@email.com" and login_form.password.data == "12345678":
+            return render_template("success.html")
+        else:
+            return render_template("denied.html")
     return render_template('login.html', form=login_form)
 
 
